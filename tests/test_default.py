a999a68deee75f4147bdf7d55979c4aea297ed14
paulfantom <pawel@krupa.net.pl>
2018-01-11 21:25:14 +0100

    move to newer testinfra fixtures

--- tests/test_default.py
+++ tests/test_default.py
@@ -3,7 +3,7 @@ from testinfra.utils.ansible_runner import AnsibleRunner
 testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 
-def test_files(File):
+def test_files(host):
     dirs = [
         "/opt/blackbox_exporter",
         "/var/log/blackbox_exporter"
@@ -13,21 +13,21 @@ def test_files(File):
         "/opt/blackbox_exporter/blackbox_exporter"
     ]
     for directory in dirs:
-        d = File(directory)
+        d = host.file(directory)
         assert d.is_directory
         assert d.exists
     for file in files:
-        f = File(file)
+        f = host.file(file)
         assert f.exists
         assert f.is_file
 
 
-def test_service(Service):
-    s = Service("blackbox_exporter")
+def test_service(host):
+    s = host.service("blackbox_exporter")
     assert s.is_running
     assert s.is_enabled
 
 
-def test_socket(Socket):
-    s = Socket("tcp://127.0.0.1:9115")
+def test_socket(host):
+    s = host.socket("tcp://127.0.0.1:9115")
     assert s.is_listening
