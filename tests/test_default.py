1a6a336134f917c63def63ca65ffdb88bc39e4dd
paulfantom <pawel@krupa.net.pl>
2018-01-11 20:54:57 +0100

    reorganize tests; test if service is running

--- tests/test_default.py
+++ tests/test_default.py
@@ -3,44 +3,31 @@ from testinfra.utils.ansible_runner import AnsibleRunner
 testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 
-def test_directories(File):
-    present = [
+def test_files(File):
+    dirs = [
         "/opt/blackbox_exporter",
         "/var/log/blackbox_exporter"
     ]
-    if present:
-        for directory in present:
-            d = File(directory)
-            assert d.is_directory
-            assert d.exists
-
-
-def test_files(File):
-    present = [
+    files = [
         "/etc/blackbox_exporter.yml",
         "/opt/blackbox_exporter/blackbox_exporter"
     ]
-    if present:
-        for file in present:
-            f = File(file)
-            assert f.exists
-            assert f.is_file
+    for directory in dirs:
+        d = File(directory)
+        assert d.is_directory
+        assert d.exists
+    for file in files:
+        f = File(file)
+        assert f.exists
+        assert f.is_file
 
 
 def test_service(Service):
-    present = [
-        "blackbox_exporter"
-    ]
-    if present:
-        for service in present:
-            s = Service(service)
-            assert s.is_enabled
+    s = Service("blackbox_exporter")
+    assert s.is_running
+    assert s.is_enabled
 
 
 def test_socket(Socket):
-    present = [
-        "tcp://127.0.0.1:9115"
-    ]
-    for socket in present:
-        s = Socket(socket)
-        assert s.is_listening
+    s = Socket("tcp://127.0.0.1:9115")
+    assert s.is_listening
