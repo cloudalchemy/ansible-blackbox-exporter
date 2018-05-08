1b054edc43e549e22a5d62079f3283c082ae2ad5
paulfantom <pawel@krupa.net.pl>
2018-05-08 16:31:20 +0200

    upgrade to molecule 2.x; fix linter; update contributor guidelines

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,6 +1,7 @@
 from testinfra.utils.ansible_runner import AnsibleRunner
 
-testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
 def test_files(host):
